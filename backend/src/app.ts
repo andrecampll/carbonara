0a18ceaaf06ff10ff901774e8511ce80eeb2389a
andrecampll <andrevictor50@gmail.com>
2024-05-22 09:54:05 -0300

    ci: adds build and start commands

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,7 @@ import { expressMiddleware } from '@apollo/server/express4'
 import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
 import cors from 'cors'
 import express from 'express'
+import { DocumentNode } from 'graphql'
 import http from 'http'
 
 import { appResolvers } from './resolvers/appResolver.js'
@@ -11,26 +12,35 @@ import { appTypeDefs } from './typeDefs/appTypeDef.js'
 const app = express()
 const httpServer = http.createServer(app)
 
-const server = new ApolloServer({
-  typeDefs: appTypeDefs,
-  resolvers: appResolvers,
-  plugins: [
-    ApolloServerPluginDrainHttpServer({
-      httpServer,
+async function startApolloServer(
+  typeDefs: DocumentNode,
+  resolvers: typeof appResolvers,
+) {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [
+      ApolloServerPluginDrainHttpServer({
+        httpServer,
+      }),
+    ],
+  })
+
+  await server.start()
+
+  app.use(
+    '/graphql',
+    cors({
+      origin: 'http://localhost:3000',
     }),
-  ],
-})
+    express.json(),
+    expressMiddleware(server),
+  )
 
-await server.start()
+  await new Promise<void>((resolve) =>
+    httpServer.listen({ port: 3333 }, resolve),
+  )
+  console.log(`🚀 Server ready at http://localhost:3333/graphql`)
+}
 
-app.use(
-  '/graphql',
-  cors({
-    origin: 'http://localhost:3000',
-  }),
-  express.json(),
-  expressMiddleware(server),
-)
-
-await new Promise<void>((resolve) => httpServer.listen({ port: 3333 }, resolve))
-console.log(`🚀 Server ready at http://localhost:3333/graphql`)
+startApolloServer(appTypeDefs, appResolvers)
